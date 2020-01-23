a920a7d1e01ef72ea7dafce023a792e275ae92d7
Dhruv Automated <dhruv.automated@gmail.com>
2020-01-23 10:32:23 -0800

    Dhruv Test

---
+++ app/src/main/java/com/example/dhruvgithubdemo/Basic_demo.java
@@ -0,0 +1,14 @@
+package com.example.dhruvgithubdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Basic_demo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_demo);
+    }
+}
