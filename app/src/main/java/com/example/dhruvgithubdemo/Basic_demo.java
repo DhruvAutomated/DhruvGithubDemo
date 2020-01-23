515750e04b418285437278343db2d85013267fc2
Dhruv Automated <dhruv.automated@gmail.com>
2020-01-23 11:06:29 -0800

    this toast add in project

--- app/src/main/java/com/example/dhruvgithubdemo/Basic_demo.java
+++ app/src/main/java/com/example/dhruvgithubdemo/Basic_demo.java
@@ -3,6 +3,7 @@ package com.example.dhruvgithubdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Basic_demo extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class Basic_demo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_demo);
+
+        Toast.makeText(Basic_demo.this);
     }
 }
