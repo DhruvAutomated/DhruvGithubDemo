18bb517cf69167d8793b4d13cc94e86e2eb25604
Dhruv Automated <dhruv.automated@gmail.com>
2020-01-23 11:37:39 -0800

    edit text added

--- app/src/main/java/com/example/dhruvgithubdemo/Admin_Activity.java
+++ app/src/main/java/com/example/dhruvgithubdemo/Admin_Activity.java
@@ -3,12 +3,20 @@ package com.example.dhruvgithubdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Admin_Activity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_);
+
+        EditText ettest = (EditText)findViewById(R.id.ettest);
+
+        ettest.setText("this is test");
+
     }
 }
