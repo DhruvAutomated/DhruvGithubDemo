f0b0dc5fa23953999fdf74a98ded029a6f1ecd1e
Dhruv Automated <dhruv.automated@gmail.com>
2020-01-27 11:35:09 -0800

    Create basic_Demo.java

---
+++ app/src/main/java/com/example/dhruvgithubdemo/basic_Demo.java
@@ -0,0 +1,32 @@
+package com.example.dhruvgithubdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Admin_Activity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_);
+
+        EditText ettest = (EditText)findViewById(R.id.ettest);
+
+        ettest.setText("this is test");
+
+        String test11 = "helloo";
+        String test22 = "hi";
+        String Testobject ="" ;
+
+        String DhruvTest = " ";
+        String Dhruv22 = " ";
+
+
+        Object object = new Object();
+
+    }
+}
